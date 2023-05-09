15495a5ee5272d2d57965639888ae34a681d471d
nengkya <nengkya@yahoo.com>
2023-05-09 11:08:27 +0700

    use Soup instead

--- sele.py
+++ sele.py
@@ -1,3 +1,5 @@
+import requests
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -22,5 +24,15 @@ option.add_argument("start-maximized");
 driver = webdriver.Edge(options = option)
 page1='https://www.mpmadirectory.org.my/all-members?limit=200&cc=p'
 driver.get(page1)
+
+# retrieve the data from URL
+response = requests.get(page1)
+
+# parse the contents of the webpage
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# filter all the <a> tags from the parsed document
+for tag in soup.find_all('a'):
+   print(tag)
 
 Pause(driver).pause(10)
