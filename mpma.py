5365407943a4c59dc4c26200f317d372997e8c85
nengkya <nengkya@yahoo.com>
2023-05-09 13:45:35 +0700

    rest my body

--- mpma.py
+++ mpma.py
@@ -15,11 +15,18 @@ soup = get_soup(page1)
 
 logging.basicConfig(filename = 'company.log', level = logging.INFO)
 
+i = 1
 #filter all the <a> tags from the parsed document
 for a in soup.find_all('a'):
 	a = a.get('href')
 
-	if '/members/' in a:
+	if not ('/members/' in a):
+		continue
+
+	if i == 4:
 		url = 'https://www.mpmadirectory.org.my' + a
 		soup = get_soup(url)
-		logging.info('getting :', url)
+		logging.info(soup)
+		print(type(soup.text))
+
+	i += 1
